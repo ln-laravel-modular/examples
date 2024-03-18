99f76b7df3956fc99e2406040f44e38660d4e48b
langnang <langnang.chen@outlook.com>
2024-03-18 13:23:21 +0800

    Module

--- Http/Controllers/ExamplesController.php
+++ Http/Controllers/ExamplesController.php
@@ -5,7 +5,7 @@ namespace Modules\Examples\Http\Controllers;
 use Illuminate\Contracts\Support\Renderable;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
-use Modules\Core\Support\Module;
+use App\Support\Module;
 
 class ExamplesController extends \App\Http\Controllers\Controller
 {
